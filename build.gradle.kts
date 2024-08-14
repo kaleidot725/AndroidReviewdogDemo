9c573d60e15b51108f8aee7563124456020db318
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-08-14 14:10:19 +0900

    feat: introduce ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.SARIF)
+        }
+    }
 }
