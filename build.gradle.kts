a3a69fcf04ba90859fd71279453cb6512995a7d2
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-08-14 14:27:42 +0900

    feat: introduce detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -7,9 +8,11 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 subprojects {
+    // Ktlint
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
         version.set("1.3.1")
@@ -23,5 +26,21 @@ subprojects {
             exclude("**/generated/**")
             include("**/kotlin/**")
         }
+    }
+}
+
+detekt {
+    toolVersion = "1.23.6"
+    buildUponDefaultConfig = true
+    basePath = projectDir.toString()
+    ignoreFailures = true
+    parallel = true
+    autoCorrect = false
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        sarif.required.set(true)
     }
 }
